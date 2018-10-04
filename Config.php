fb44fd73a07048d7b8d5fd1a3f50d9ab275f0517
Donald Pakkies <donaldpakkies@gmail.com>
2018-10-04 11:18:57 +0200

    Feat: config file for env and db

---
+++ Config.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Modulus\System;
+
+class Config
+{
+  /**
+   * Environment config
+   *
+   * @var $environment
+   */
+  public static $environment;
+
+  /**
+   * Database config
+   *
+   * @var $database
+   */
+  public static $database;
+
+  /**
+   * Application root
+   *
+   * @var $root
+   */
+  public static $root;
+
+  /**
+   * Application env
+   *
+   * @var $env
+   */
+  public static $env;
+}
